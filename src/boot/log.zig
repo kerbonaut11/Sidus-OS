9c40e84dd4c3d76327294535b237e9f1f918cbfa
kerbonaut11 <tobiasfetzer09@gmx.de>
2026-02-09 19:20:36 +0100

    Hello, World!

---
+++ src/boot/log.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+const log = std.log;
+const uefi = std.os.uefi;
+const Writer = std.Io.Writer;
+
+pub var stdout: Writer = .{
+    .buffer = &.{},
+    .vtable = &.{
+        .drain = drain,
+    },
+};
+
+pub fn logFn(
+    comptime message_level: log.Level,
+    comptime scope: @TypeOf(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    stdout.print("{s}", .{message_level.asText()}) catch {};
+    if (scope != .default) stdout.print("@{s}", .{@tagName(scope)}) catch {};
+    stdout.print(": "++format, args) catch {};
+    stdout.flush() catch {};
+}
+
+fn drain(w: *Writer, data: []const []const u8, splat: usize) Writer.Error!usize {
+    var bytes_written = w.end;
+    try outStr(w.buffer[0..w.end]);
+    w.end = 0;
+
+    for (data[0..data.len-1]) |str| {
+        try outStr(str);
+        bytes_written += str.len;
+    }
+
+    const splat_str = data[data.len-1];
+    for (0..splat) |_| try outStr(splat_str);
+    bytes_written += splat*splat_str.len;
+    
+    return bytes_written;
+}
+
+fn outStr(str: []const u8) Writer.Error!void {
+    for (str) |ch| try outCh(ch);
+}
+
+fn outCh(ch: u8) Writer.Error!void {
+    if (ch == '\n') {
+        try outRaw('\n');
+        try outRaw('\r');
+    } else {
+        try outRaw(ch);
+    }
+}
+
+fn outRaw(byte: u8) Writer.Error!void {
+    const out = uefi.system_table.con_out orelse return error.WriteFailed;
+    if (!(out.outputString(&.{byte}) catch false)) return error.WriteFailed;
+}
+
--- src/boot/main.zig
+++ src/boot/main.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+pub const std_options: std.Options = .{
+    .logFn = @import("log.zig").logFn,
+};
 const uefi = std.os.uefi;
 const GraphicsOutput = uefi.protocol.GraphicsOutput;
 
@@ -10,35 +13,37 @@ pub fn uefiString(comptime str: []const u8) [:0]const u16 {
 }
 
 pub fn main() uefi.Error!void {
-    const out = uefi.system_table.con_out.?;
+    std.log.debug("Hello, World!", .{});
+
+    try uefi.system_table.boot_services.?.stall(5*1000*1000);
+    //const out = uefi.system_table.con_out.?;
     //const in = uefi.system_table.con_in.?;
-    const boot_services = uefi.system_table.boot_services.?;
+    //const boot_services = uefi.system_table.boot_services.?;
 
-    const fs = (try boot_services.locateProtocol(uefi.protocol.SimpleFileSystem, null)).?;
-    const volume = try fs.openVolume();
-    const kernel = try volume.open(uefiString("EFI"), .read, .{});
-    _ = kernel;
+    //const fs = (try boot_services.locateProtocol(uefi.protocol.SimpleFileSystem, null)).?;
+    //const volume = try fs.openVolume();
+    //const kernel = try volume.open(uefiString("kernel"), .read, .{});
+    //_ = kernel;
 
-    const graphics = (try boot_services.locateProtocol(GraphicsOutput, null)).?;
-    const frame_buffer: []u8 = @as([*]u8, @ptrFromInt(graphics.mode.frame_buffer_base))[0..graphics.mode.frame_buffer_size];
+    //const graphics = (try boot_services.locateProtocol(GraphicsOutput, null)).?;
+    //const frame_buffer: []u8 = @as([*]u8, @ptrFromInt(graphics.mode.frame_buffer_base))[0..graphics.mode.frame_buffer_size];
 
-    var memory_map: [*]uefi.tables.MemoryDescriptor = undefined;
-    var memory_map_size: usize = 0;
-    var memory_map_key: uefi.tables.MemoryMapKey = undefined;
-    var descriptor_size: usize = undefined;
-    var descriptor_version: u32 = undefined;
-    std.debug.assert(boot_services._getMemoryMap(&memory_map_size, @ptrCast(&memory_map), &memory_map_key, &descriptor_size, &descriptor_version) == .buffer_too_small);
+    //var memory_map: [*]uefi.tables.MemoryDescriptor = undefined;
+    //var memory_map_size: usize = 0;
+    //var memory_map_key: uefi.tables.MemoryMapKey = undefined;
+    //var descriptor_size: usize = undefined;
+    //var descriptor_version: u32 = undefined;
+    //std.debug.assert(boot_services._getMemoryMap(&memory_map_size, @ptrCast(&memory_map), &memory_map_key, &descriptor_size, &descriptor_version) == .buffer_too_small);
 
-    const memory_map_slice = try boot_services.getMemoryMap(try boot_services.allocatePool(.boot_services_data, memory_map_size));
-    var iter = memory_map_slice.iterator();
-    var fmt_buf: [1024]u8 = undefined;
-    while (iter.next()) |e| {
-        const fmt = std.fmt.bufPrint(&fmt_buf, "{x}..{x}\r\n", .{e.physical_start, e.physical_start+e.number_of_pages*4096}) catch unreachable;
-        for (fmt) |ch| _ = try out.outputString(&.{ch, 0});
-    }
+    //const memory_map_slice = try boot_services.getMemoryMap(try boot_services.allocatePool(.boot_services_data, memory_map_size));
+    //var iter = memory_map_slice.iterator();
+    //var fmt_buf: [1024]u8 = undefined;
+    //while (iter.next()) |e| {
+        //const fmt = std.fmt.bufPrint(&fmt_buf, "{x}..{x}\r\n", .{e.physical_start, e.physical_start+e.number_of_pages*4096}) catch unreachable;
+        //for (fmt) |ch| _ = try out.outputString(&.{ch, 0});
+    //}
 
-    try boot_services.exitBootServices(uefi.handle, memory_map_slice.info.key);
+    //try boot_services.exitBootServices(uefi.handle, memory_map_slice.info.key);
 
-    @memset(frame_buffer, 0xff);
-    //try uefi.system_table.boot_services.?.stall(5*1000*1000);
+    //@memset(frame_buffer, 0xff);
 }
