b790a1636677c36ef37c0e3615aaeb9a6c74d372
kerbonaut11 <tobiasfetzer09@gmx.de>
2026-02-09 20:44:20 +0100

    start on kernel loader

---
+++ src/boot/load_kernel.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const log = std.log.scoped(.load_kernel);
+const uefi = std.os.uefi;
+const File = uefi.protocol.File;
+const elf = std.elf;
+const util = @import("util.zig");
+const readAll = util.readAll;
+const readOne = util.readOne;
+
+const Ehdr = elf.Elf64_Ehdr;
+const Phdr = elf.Elf64_Phdr;
+const Shdr = elf.Elf64_Shdr;
+
+const Error = error{BadFile}||uefi.Error;
+
+pub fn loadKernel() Error!void {
+    const boot_services = uefi.system_table.boot_services.?;
+    const fs = (try boot_services.locateProtocol(uefi.protocol.SimpleFileSystem, null)).?;
+    const volume = try fs.openVolume();
+    const kernel = try volume.open(util.uefiStringLit("kernel"), .read, .{});
+    try load(kernel);
+}
+
+fn load(file: *File) Error!void {
+    const header: Ehdr = try readOne(file, Ehdr);
+    log.debug("{}", .{header.e_machine});
+
+    for (0..header.e_phnum) |i| {
+        try file.setPosition(header.e_phoff + i*header.e_phentsize);
+        const phdr = try readOne(file, Phdr);
+        log.debug("{}", .{phdr});
+    }
+}
---
+++ src/boot/util.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const uefi = std.os.uefi;
+const File = uefi.protocol.File;
+
+pub fn uefiStringLit(comptime str: []const u8) [:0]const u16 {
+    comptime var out: []const u16 = &.{};
+    inline for (str) |ch| out = out ++ &[1]u16{ch};
+    out = out ++ &[1]u16{0};
+    return @ptrCast(out);
+}
+
+
+pub fn readAll(file: *File, out: []u8) !void {
+    if (try file.read(out) != out.len) {
+        return error.BadFile;
+    }
+}
+
+pub fn readOne(file: *File, comptime T: type) !T {
+    var val: T = undefined;
+    try readAll(file, @ptrCast(&val));
+    return val;
+}
