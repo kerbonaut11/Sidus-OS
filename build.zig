2537bf2195952ae47a625c6c937eb8b99c16fac8
kerbonaut11 <tobiasfetzer09@gmx.de>
2026-02-09 18:52:30 +0100

    first commit

---
+++ build.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+const Build = std.Build;
+
+pub fn build(b: *Build) void {
+    const boot_loader = buildBootloader(b);
+    const kernel = buildKernel(b);
+
+    const image_type = b.option(enum {img, gpt, iso}, "image-type", "format of the bootable output file") 
+        orelse .img;
+    const ovmf_path = b.option([]const u8, "omvf", "path to the ovmf bios") 
+        orelse "/usr/share/ovmf/x64/OVMF.4m.fd";
+
+    var make_image = b.addSystemCommand(&.{"sh"});
+    make_image.addFileArg(b.path("mkimage.sh"));
+    make_image.addArg(@tagName(image_type));
+    const fat_img = make_image.addOutputFileArg("fat.img");
+    make_image.addArtifactArg(boot_loader);
+    make_image.addArtifactArg(kernel);
+    const image = switch (image_type) {
+        .gpt => make_image.addOutputFileArg("hdimage.bin"),
+        .img => fat_img,
+        .iso => make_image.addOutputFileArg("cdimage.iso"),
+    };
+
+    var qemu_cmd = b.addSystemCommand(&.{"qemu-system-x86_64"});
+    qemu_cmd.addArgs(&.{"-smbios", "type=0,uefi=on"});
+    qemu_cmd.addArgs(&.{"-bios", ovmf_path});
+    switch (image_type) {
+        .gpt, .img => qemu_cmd.addArg("-hda"),
+        .iso => qemu_cmd.addArg("-cdrom"),
+    }
+    qemu_cmd.addFileArg(image);
+
+    const run_step = b.step("run", "run HD image with QEMU");
+    run_step.dependOn(&qemu_cmd.step);
+
+    const install_file_name = switch (image_type) {
+        .gpt => "hdimage.bin",
+        .img => "fat.img",
+        .iso => "cdimage.iso",
+    };
+    const install_file = b.addInstallFile(image, install_file_name);
+    b.getInstallStep().dependOn(&install_file.step);
+}
+
+pub fn buildBootloader(b: *Build) *Build.Step.Compile {
+    return b.addExecutable(.{
+        .name = "boot-loader",
+        .root_module = b.addModule("boot", .{
+            .root_source_file = b.path("src/boot/main.zig"),
+            .target = b.resolveTargetQuery(.{
+                .abi = .msvc,
+                .cpu_arch = .x86_64,
+                .os_tag = .uefi,
+            })
+        })
+    });
+}
+
+pub fn buildKernel(b: *Build) *Build.Step.Compile {
+    return b.addExecutable(.{
+        .name = "kernel",
+        .root_module = b.addModule("kernel", .{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(.{
+                .cpu_arch = .x86_64,
+                .os_tag = .freestanding,
+            }),
+            .code_model = .kernel,
+        }),
+    });
+}
---
+++ mkimage.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+mode=$1
+img=$2
+boot_loader_elf=$3
+kernel_elf=$4
+out=$5
+
+dd if=/dev/zero of=$img bs=1k count=28000
+mkfs.vfat $img
+loop_dev=$(udisksctl loop-setup -f $img)
+loop_dev="${loop_dev##* }"
+loop_dev=${loop_dev::-1}
+>&2 echo "loop dev:"
+>&2 echo $loop_dev
+
+
+img_mnt=$(udisksctl mount -b $loop_dev)
+img_mnt="${img_mnt##* }"
+>&2 echo "img mnt:"
+>&2 echo $img_mnt
+
+mkdir $img_mnt/EFI
+mkdir $img_mnt/EFI/BOOT
+cp $boot_loader_elf $img_mnt/EFI/BOOT/BOOTX64.EFI
+cp $kernel_elf $img_mnt/kernel
+
+case $mode in
+  gpt)
+    mkgpt -o $out --image-size 4096 --part $img --type system
+    ;;
+  iso)
+    mkdir iso
+    cp $img iso
+    xorriso -as mkisofs -R -f -e fat.img -no-emul-boot -o $out iso
+    rm -r iso
+    ;;
+  img)
+    ;;
+esac
+
+udisksctl unmount -b $loop_dev
+udisksctl loop-delete -b $loop_dev
