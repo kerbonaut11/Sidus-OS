68edd70c946ae017418ef186a609cf36372ca41b
kerbonaut11 <tobiasfetzer09@gmx.de>
2026-02-13 18:07:50 +0100

    fixed outl and inl

--- src/kernel/io.zig
+++ src/kernel/io.zig
@@ -12,9 +12,9 @@ pub fn outw(port: u16, val: u16) void {
     );
 }
 
-pub fn outd(port: u16, val: u32) void {
+pub fn outl(port: u16, val: u32) void {
     asm volatile (
-        \\outd %[val], %[port]
+        \\outl %[val], %[port]
         :: [port] "N{dx} "(port), [val] "{eax}" (val),
     );
 }
@@ -28,14 +28,14 @@ pub fn inb(port: u16) u8 {
 
 pub fn inw(port: u16) u16 {
     return asm volatile (
-        \\inb %[port], %[val]
+        \\inw %[port], %[val]
         : [val] "={ax}" (->u16) : [port] "N{dx} "(port)
     );
 }
 
-pub fn ind(port: u16) u32 {
+pub fn inl(port: u16) u32 {
     return asm volatile (
-        \\inb %[port], %[val]
+        \\inl %[port], %[val]
         : [val] "={eax}" (->u32) : [port] "N{dx} "(port)
     );
 }
